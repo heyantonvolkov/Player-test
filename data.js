30f9b3b909d77e52cb9ff5acef491da3279afa27
lalalayousuck <tonyfirehead@gmail.com>
2016-07-01 12:57:59 +0300

    JS new

---
+++ data.js
@@ -0,0 +1,32 @@
+var songs = [
+  {
+    track: "We are the champions", band: "qween", imgUrl: "/img/songs/song1-back.png", songUrl: ""
+  },
+  {
+    track: "Don't move", band: "Masha", imgUrl: "/img/songs/song2-back.png", songUrl: ""
+  },
+  {
+    track: "Qwadrocopter", band: "KupiProdai", imgUrl: "/img/songs/song3-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "/img/songs/song4-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song5-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song6-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song7-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song8-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song9-back.png", songUrl: ""
+  },
+  {
+    track: "We are the champions", band: "qween", imgUrl: "img/songs/song10-back.png", songUrl: ""
+  }
+];
--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+  var blockIndex = 0;
+
   $(".hero-songblock-play").click(function(){
     $(".hero-songblock").removeClass("is-playing");
-    $(this).find(".hero-songblock").addClass("is-playing");
+    blockIndex = getBlockIndex();
+    console.log(blockIndex);
+    setSongInfo(songs[2].track, songs[2].band, songs[2].imgUrl);
   });
+
+  
+  function getBlockIndex () {
+    return $(this).parent().index();
+  }
+
+  function setSongInfo(track, band, imgUrl) {
+    $(".footer-songname").text(track);
+    $(".footer-singer").text(band);
+    $(".footer-song-pic").attr(src, imgUrl);
+  }
+
+  
 });
