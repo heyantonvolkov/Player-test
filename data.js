3288b76756e7cdac76c2b8dfff9e179cbedc3258
lalalayousuck <tonyfirehead@gmail.com>
2016-07-01 21:34:50 +0300

    is-plaing + is-paused

--- data.js
+++ data.js
@@ -2,67 +2,61 @@ var songs = [
   {
     track: 'We are the champions', 
     band: 'qween', 
-    imgUrl: '/img/songs/song1-back.png', 
+    imgUrl: 'img/songs/song1-back.png', 
     songJenre: 'Pop Music'
   },
   {
     track: 'Rich Bitch', 
     band: 'Doors', 
-    imgUrl: '/img/songs/song2-back.png', 
+    imgUrl: 'img/songs/song2-back.png', 
     songJenre: 'Adult Music'
   },
   {
     track: 'Paper Sumit', 
     band: 'Tuuber', 
-    imgUrl: '/img/songs/song3-back.png', 
+    imgUrl: 'img/songs/song3-back.png', 
     songJenre: 'Rock Music'
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song4-back.png", 
+    imgUrl: "img/songs/song4-back.png", 
     songJenre: "Hip Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song5-back.png", 
+    imgUrl: "img/songs/song5-back.png", 
     songJenre: "Man Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song6-back.png", 
+    imgUrl: "img/songs/song6-back.png", 
     songJenre: "Sugar Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song7-back.png", 
+    imgUrl: "img/songs/song7-back.png", 
     songJenre: "Sucker Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song8-back.png", 
+    imgUrl: "img/songs/song8-back.png", 
     songJenre: "Bitchy Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song9-back.png", 
+    imgUrl: "img/songs/song9-back.png", 
     songJenre: "Hamm Music"
   },
   {
     track: "We are the champions", 
     band: "qween", 
-    imgUrl: "/img/songs/song10-back.png", 
+    imgUrl: "img/songs/song10-back.png", 
     songJenre: "Thor Music"
-  },
-  {
-    track: "We are the champions", 
-    band: "qween", 
-    imgUrl: "/img/songs/song1-back.png", 
-    songJenre: "Pop Music"
   }
 ];
--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 $(document).ready(function() {
 
 
-  var theTemplateScript = $("#song-template").html();
-
-  // Compile the template
-  var theTemplate = Handlebars.compile(theTemplateScript);
-
-  // Define our data object
-  var context= songs;
-  console.log(context[5]);
-  var theCompiledHtml = theTemplate(context);
-
-  $('.hero .container').append(theCompiledHtml);
-
+  
+  var songTemplate = Handlebars.compile($("#song-template").html());
+  $('.hero .container').html(songTemplate({songs: songs}));
+ 
 
   $(".hero-songblock-play").click(function(){
-    $(".hero-songblock").removeClass("is-playing");
     var blockIndex = $(this).parent().index();
-    $(".hero-songblock").eq(blockIndex).addClass("is-playing");
-    setSongInfo(songs[blockIndex].track, songs[blockIndex].band, songs[blockIndex].imgUrl);
+    if ($(".hero-songblock").eq(blockIndex).hasClass("is-paused")) {
+      $(".hero-songblock").eq(blockIndex).removeClass("is-paused");
+      $(".hero-songblock").eq(blockIndex).addClass("is-playing");
+    } else {
+      $(".hero-songblock").removeClass("is-playing is-paused");
+      $(".hero-songblock").eq(blockIndex).addClass("is-playing");
+      setSongInfo(songs[blockIndex].track, songs[blockIndex].band, songs[blockIndex].imgUrl);
+    }
   });
-
 
   function setSongInfo(track, band, imgUrl) {
     $(".footer-songname").text(track);
     $(".footer-singer").text(band);
-    $(".footer-song-pic").attr(src, imgUrl);
+    $(".footer-song-pic").attr(src, 'imgUrl');
   }
 
+  $(".hero-songblock-pause").click(function(){
+    var blockIndex = $(this).parent().index();
+    $(".hero-songblock").eq(blockIndex).addClass("is-paused");
+    $(".hero-songblock").eq(blockIndex).removeClass("is-playing");
+  });
 
 
 });
