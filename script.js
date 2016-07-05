9362118be013e482d2311127207e3ab864c3dc58
lalalayousuck <tonyfirehead@gmail.com>
2016-07-05 14:40:42 +0300

    jquery broken 1

--- script.js
+++ script.js
@@ -5,102 +5,149 @@ $(document).ready(function() {
   var page = 0;
   var perpage = 10;
 
+  $('#slider').rangeslider();
 
+  $.get(
+    "http://api.radioreddit.com/tracks",
+    function(json) {
+      songInfo = json;
 
-  $('#slider').slider({
-    min: 0,
-    max: 100,
-    value: 35
-  });
+      songs = songInfo.map(
+        function(song) {
+          return {
+            album: song.Album.title,
+            band: song.Artist.name,
+            path: song.media.original,
+            track: song.title,
+            imgUrl: "https://source.unsplash.com/random"
+          }
+        });
 
+      $.get(
+        "https://unsplash.it/list",
+        function(json) {
+          imagesInfo = json;
 
-  $.get("http://api.radioreddit.com/tracks", function(json){
-    songInfo = json;
+          images = imagesInfo.map(
+            function(image) {
+              return {
+                imgUrl: image
+                  .post_url
+              }
+            });
 
-    songs = songInfo.map(function(song){
-      return  {
-        album: song.Album.title,
-        band: song.Artist.name,
-        path: song.media.original,
-        track: song.title,
-        imgUrl: "https://source.unsplash.com/random"
-      }
+          var imagesHundred =
+            images.slice(0, 100);
+
+          for (var i = 0; i <
+            100; ++i) {
+            songs[i].imgUrl =
+              images[i].imgUrl;
+          }
+        });
+
+      var songs1 = sliceSongs(
+        songs);
+
+      var songTemplate =
+        Handlebars.compile($(
+          "#song-template").html());
+      $('.hero .container').html(
+        songTemplate({
+          songs: songs1
+        }));
+
+      var playerTemplate =
+        Handlebars.compile($(
+          "#player-ui-template"
+        ).html());
+      $('.footer-song-info').html(
+        playerTemplate(songs[0])
+      );
     });
 
-    $.get("https://unsplash.it/list", function(json){
-      imagesInfo = json;
-
-      images = imagesInfo.map(function(image){
-        return {
-          imgUrl: image.post_url
-        }
-      });
-      
-      var imagesHundred = images.slice(0,100);
-
-      for (var i=0; i<100; ++i){
-        songs[i].imgUrl = images[i].imgUrl;
-      }
-
-      console.log(songs);
-
-    });
-
-    console.log(songs);
-
-    var songs1 = sliceSongs(songs);
-
-    var songTemplate = Handlebars.compile($("#song-template").html());
-    $('.hero .container').html(songTemplate({songs: songs1}));
-
-    var playerTemplate = Handlebars.compile($("#player-ui-template").html());
-    $('.footer-song-info').html(playerTemplate(songs[0]));
-  });
-  
-  function sliceSongs (array) {
-    var result = array.slice(page*perpage, perpage+(page*perpage));
+  function sliceSongs(array) {
+    var result = array.slice(page *
+      perpage, perpage + (page *
+        perpage));
     ++page
     return result;
   }
- 
-  $(".footer-btn-new").click(function(){
-    var moreSongs = sliceSongs(songs);
-    console.log(moreSongs);
-    var songTemplate = Handlebars.compile($("#song-template").html());
-    $('.hero .container').append(songTemplate({songs: moreSongs}));
-  });
 
-  $(".container").on('click', ".hero-songblock-play", function() {
-    blockIndex = $(this).parent().index();
-    if ($(".hero-songblock").eq(blockIndex).hasClass("is-paused")) {
-      $(".hero-songblock").eq(blockIndex).removeClass("is-paused");
-      $(".hero-songblock").eq(blockIndex).addClass("is-playing");
-      play();
-    } else {
-      $(".hero-songblock").removeClass("is-playing is-paused");
-      $(".hero-songblock").eq(blockIndex).addClass("is-playing");
-      setSongInfo();
-      play();
-    }
-  });
+  $(".footer-btn-new").click(
+    function() {
+      var moreSongs = sliceSongs(
+        songs);
+      console.log(moreSongs);
+      var songTemplate =
+        Handlebars.compile($(
+          "#song-template").html());
+      $('.hero .container').append(
+        songTemplate({
+          songs: moreSongs
+        }));
+    });
+
+  $(".container").on('click',
+    ".hero-songblock-play",
+    function() {
+      blockIndex = $(this).parent()
+        .index();
+      if ($(".hero-songblock").eq(
+          blockIndex).hasClass(
+          "is-paused")) {
+        $(".hero-songblock").eq(
+          blockIndex).removeClass(
+          "is-paused");
+        $(".hero-songblock").eq(
+          blockIndex).addClass(
+          "is-playing");
+        play();
+      } else {
+        $(".hero-songblock").removeClass(
+          "is-playing is-paused"
+        );
+        $(".hero-songblock").eq(
+          blockIndex).addClass(
+          "is-playing");
+        setSongInfo();
+        play();
+      }
+    });
 
   function setSongInfo() {
-    var playerTemplate = Handlebars.compile($("#player-ui-template").html());
-    $('.footer-song-info').html(playerTemplate(songs[blockIndex]));
+    var playerTemplate = Handlebars
+      .compile($(
+        "#player-ui-template").html());
+    $('.footer-song-info').html(
+      playerTemplate(songs[
+        blockIndex]));
   }
 
-  $(".container").on('click', ".hero-songblock-pause", function(){
-    blockIndex = $(this).parent().index();
-    $(".hero-songblock").eq(blockIndex).addClass("is-paused");
-    $(".hero-songblock").eq(blockIndex).removeClass("is-playing");
-    pause();
-  });
+  $(".container").on('click',
+    ".hero-songblock-pause",
+    function() {
+      blockIndex = $(this).parent()
+        .index();
+      $(".hero-songblock").eq(
+        blockIndex).addClass(
+        "is-paused");
+      $(".hero-songblock").eq(
+        blockIndex).removeClass(
+        "is-playing");
+      pause();
+    });
 
-  $(".footer-pause-btn").click(function(){
-    pause();
-    $(".hero-songblock").eq(blockIndex).addClass("is-paused");
-    $(".hero-songblock").eq(blockIndex).removeClass("is-playing");
-  });
+  $(".footer-pause-btn").click(
+    function() {
+      pause();
+      $(".hero-songblock").eq(
+        blockIndex).addClass(
+        "is-paused");
+      $(".hero-songblock").eq(
+        blockIndex).removeClass(
+        "is-playing");
+    });
 
   function play() {
     var audio = $('#sound')[0];
@@ -111,6 +158,6 @@ $(document).ready(function() {
     var audio = $('#sound')[0];
     audio.pause();
   }
-  
-  
+
+
 });
